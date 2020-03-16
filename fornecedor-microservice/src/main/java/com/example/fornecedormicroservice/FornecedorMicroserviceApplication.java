436ae2a6e0e3741ef98f85d2f154f395bf2d73f1
DANIEL COSTA CUNHA <daniel.cunha@itbam.org.br>
2020-03-16 13:41:21 -0400

    commit inicial

---
+++ fornecedor-microservice/src/main/java/com/example/fornecedormicroservice/FornecedorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fornecedormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedorMicroserviceApplication.class, args);
+	}
+
+}
---
+++ fornecedor-microservice/src/test/java/com/example/fornecedormicroservice/FornecedorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fornecedormicroservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
